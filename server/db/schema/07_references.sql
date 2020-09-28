7994918024d6b96d4c8c9be7eb47581de28f51d1
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-09-28 11:47:11 -0400

    Seed languages, tweak references to cascade

--- server/db/schema/07_references.sql
+++ server/db/schema/07_references.sql
@@ -1,11 +1,11 @@
-ALTER TABLE "game_players" ADD FOREIGN KEY ("player_id") REFERENCES "users" ("id");
+ALTER TABLE "game_players" ADD FOREIGN KEY ("player_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "game_players" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("id");
+ALTER TABLE "game_players" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "scores" ADD FOREIGN KEY ("player_id") REFERENCES "users" ("id");
+ALTER TABLE "scores" ADD FOREIGN KEY ("player_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "scores" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("id");
+ALTER TABLE "scores" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "game_languages" ADD FOREIGN KEY ("language_id") REFERENCES "languages" ("id");
+ALTER TABLE "game_languages" ADD FOREIGN KEY ("language_id") REFERENCES "languages" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "game_languages" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("id");
+ALTER TABLE "game_languages" ADD FOREIGN KEY ("game_id") REFERENCES "games" ("id") ON DELETE CASCADE;
