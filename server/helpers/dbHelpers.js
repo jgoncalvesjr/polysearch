2f0047d2ecd82f0a1730b09a56d4a443af8175eb
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-09-28 18:52:54 -0400

    Create languages routes, mocks, helpers for mocks - still need to improve helper logic

--- server/helpers/dbHelpers.js
+++ server/helpers/dbHelpers.js
@@ -10,7 +10,34 @@ module.exports = (db) => {
       .catch((err) => err);
   };
 
+  const getLanguages = () => {
+    const query = {
+      text: 'SELECT * FROM languages',
+    };
+
+    return db
+      .query(query)
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
+  const addUser = (username, email, password, avatar) => {
+    const query = {
+      text: `INSERT INTO users (username, email, password, avatar, multiplayer_wins)
+             VALUES $1, $2, $3, $4, 0
+             RETURNING *`,
+      values: [username, email, password, avatar]
+    };
+
+    return db
+      .query(query)
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
   return {
     getUsers,
+    getLanguages,
+    addUser
   };
 };
