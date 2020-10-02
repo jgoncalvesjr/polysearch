d47d2b74626a27ebf3b3bc5eb03cb584bbd06448
Ryan Hurtis <rhurtis@hotmail.com>
2020-10-02 13:39:11 -0400

    Added clickable menus/drop downs.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,62 @@
-import React from "react";
-import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
-import {Menu} from '@material-ui/icons';
+import React, { useState } from "react";
+import {AppBar, Toolbar, IconButton, Typography, Button, Drawer} from '@material-ui/core';
+import { Menu, AccountCircle } from '@material-ui/icons';
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  const [anchor, setAnchor] = useState('left');
+  
+  const handleDrawer = () => {
+    setAnchor('left');
+    setOpen(true);
+  };
+
+  const handleAccount = () => {
+    setAnchor('bottom');
+    setOpen(true);
+  }
+
   return (
     <div>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton edge='start' aria-label='menu'>
+          <IconButton onClick={handleDrawer} color='inherit' edge='start' aria-label='menu'>
             <Menu/>
           </IconButton>
+          <Typography variant='h6' style={{ flexGrow :1 }}>
+            PolySearch
+          </Typography>
+          <Button color='inherit'>
+            Login
+          </Button>
+          <Button color='inherit'>
+            Register
+          </Button>
+          <IconButton onClick={handleAccount} color='inherit'aria-label='account'>
+            <AccountCircle/>
+          </IconButton>
+
+
         </Toolbar>
       </AppBar>
+
+      <Drawer 
+      anchor={anchor}
+      open={open}
+      onClose={() => setOpen(false)}
+      >
+        <div style={{ height : "100%", padding : "20px"}}>
+          { anchor === "left" ?
+            <div>
+              <h5> This is the left drawer</h5>
+            </div>
+            :
+            <div>
+              <h5>This is the bottom drawer</h5>
+            </div>
+        }
+        </div>
+      </Drawer>
 
     </div>
   )
