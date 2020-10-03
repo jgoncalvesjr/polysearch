c518a7bca84948ca045cbd587fac032137d25d5e
Ryan Hurtis <rhurtis@hotmail.com>
2020-10-03 18:22:26 -0400

    Used state to store the data from the axios request to show difficulty.

--- client/src/components/JoinGame.jsx
+++ client/src/components/JoinGame.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -6,17 +6,62 @@ export default function JoinGame(props) {
 
   const { gameid } = useParams();
   console.log(gameid);
+  
+  const [dataNew, setDataNew] = useState({});
+
+  
+  // buttons for the game lobby page.
+  function startGameButton() {
+    alert('You pressed the Start Game Button!');
+  };
+
+  function mainMenuButton() {
+    document.location.href = "/";
+  };
+
+  // Variable for storing the data from the axios request.
+  //let dataRetrieved = [];
 
   useEffect(() => {
     axios({
       method: 'GET',
       url: `/api/games/${gameid}`
     }).then(({ data }) => {
-      console.log(data);
+      console.log('Here is the data being retrieved: ',data);
+      console.log('using the JoinGame functional component.')
+      
+      // trying to make the data from the axios get request retrievable by all everything.
+      setDataNew(data);
     })
       .catch(err => console.log(err));
   }, [gameid]);
 
-  return <></>;
+  console.log('here is the actual data', dataNew)
+  return (
+  <main id="multiplayer-lobby">
+  <div id="multiplayer-lobby-box">
+    <h2 id="multiplayer-lobby-title">Multiplayer Game Lobby</h2>
+    <div id="">List of Players Go Here</div>
+    <h3 id="lobby-url">Use this link to invite people to play: {dataNew.link}</h3>
+    <div>
+      <h3>Difficulty: {dataNew.mode} </h3>
+      <h3>Game Mode: </h3>
+    </div>
+    <div id='multiplayer-lobby-button-group'>
+      <button className="multiplayer-lobby-buttons" onClick={startGameButton}>Start Game</button>
+      <button className="multiplayer-lobby-buttons" onClick={mainMenuButton}>Cancel</button>
+    </div>
+  </div>
+</main> 
+  )
   
 }
+/*
+Currently this page will get the 
+ -difficulty 
+ -game mode
+Need to get it to display: 
+  -the players that are currently in the game.
+  - chat
+
+*/
