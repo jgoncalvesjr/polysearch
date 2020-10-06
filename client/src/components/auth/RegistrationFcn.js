b512d90f8b32a81a7de6a5366becbb2a53fb677a
Ryan Hurtis <rhurtis@hotmail.com>
2020-10-06 11:49:28 -0400

    Updated registration page. Removed game related localStorage variables on the home page.

--- client/src/components/auth/RegistrationFcn.js
+++ client/src/components/auth/RegistrationFcn.js
@@ -2,13 +2,58 @@ import React, { useState } from 'react';
 import axios from "axios";
 import {useHistory} from 'react-router-dom';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        PolySearch
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export default function RegistrationFcn(props) {
+  const classes = useStyles();
+
+
   const history = useHistory()
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [username, setUsername] = useState('');
-  // const [avatar, setAvatar] = useState('');
-  // const [registrationErrors, setRegistrationErrors] = useState('');
 
   // handleChange for email input field.
   const handleEmailInput = e => {
@@ -54,44 +99,136 @@ export default function RegistrationFcn(props) {
   
 
   return (
-    <div>
-    <form onSubmit={registerButton}>
-      <input 
-      type="email"
-      name="email" 
-      placeholder="Email" 
-      value={props.email} 
-      onChange={handleEmailInput} 
-      required />
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form onSubmit={registerButton} className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                onChange={handleUsernameInput} 
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="Username"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                onChange={handleAvatarInput}
+                variant="outlined"
+                
+                fullWidth
+                id="lastName"
+                label="Avatar"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={handleEmailInput}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={handlePasswordInput}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            {/* <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid> */}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
 
-      <input 
-      type="password"
-      name="password" 
-      placeholder="Password" 
-      value={props.password} 
-      onChange={handlePasswordInput} 
-      required />
+    //old stuff
+  //   <div>
+  //   <form onSubmit={registerButton}>
+  //     <input 
+  //     type="email"
+  //     name="email" 
+  //     placeholder="Email" 
+  //     value={props.email} 
+  //     onChange={handleEmailInput} 
+  //     required />
 
-      <input 
-      type="username"
-      name="username" 
-      placeholder="Username" 
-      value={props.username} 
-      onChange={handleUsernameInput} 
-      required />
+  //     <input 
+  //     type="password"
+  //     name="password" 
+  //     placeholder="Password" 
+  //     value={props.password} 
+  //     onChange={handlePasswordInput} 
+  //     required />
 
-      <input 
-      type="avatar"
-      name="avatar" 
-      placeholder="Avatar" 
-      value={props.avatar} 
-      onChange={handleAvatarInput}
-      /> 
+  //     <input 
+  //     type="username"
+  //     name="username" 
+  //     placeholder="Username" 
+  //     value={props.username} 
+  //     onChange={handleUsernameInput} 
+  //     required />
 
-      <button type="submit" >Register</button>
+  //     <input 
+  //     type="avatar"
+  //     name="avatar" 
+  //     placeholder="Avatar" 
+  //     value={props.avatar} 
+  //     onChange={handleAvatarInput}
+  //     /> 
 
-    </form>
-  </div>
+  //     <button type="submit" >Register</button>
+
+  //   </form>
+  // </div>
   );
 
 
